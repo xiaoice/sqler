147297ec3395e01981320e89fd7044ed3dd5edd6
曾小斌 <xiaoice@users.noreply.github.com>
2014-04-04 18:02:53 +0800

    Update SqlConnection.java

--- src/com/jspjs/sqler/service/SqlConnection.java
+++ src/com/jspjs/sqler/service/SqlConnection.java
@@ -4,19 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import org.apache.ibatis.session.SqlSession;
-
 import com.jspjs.sqler.dto.JdbcDto;
 
 public class SqlConnection {
-	//关闭本地ibatis数据库连接
-	public void closeSqlSession(SqlSession sqlSession){
-		if (sqlSession != null) {
-			sqlSession.close();
-			sqlSession = null;
-		}
-	}
-	
 	//获取JDBC连接
 	public Connection getConnection(JdbcDto jdbcDto) throws ClassNotFoundException, SQLException{
 		Class.forName(jdbcDto.getDriver());
